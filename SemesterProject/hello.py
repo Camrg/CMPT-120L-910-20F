dcd0a44e76e9cca8df86fc90c8bc6b80d64ed741
Camrg <70598949+Camrg@users.noreply.github.com>
2020-11-10 17:11:11 -0500

    Lab 6

--- SemesterProject/hello.py
+++ SemesterProject/hello.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Goodbye Cruel World'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+
+
+
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('')
+# def hello_world():
+#     return 'Hello, World!'
