dc651e12b2cd8829737842958da6e9602873c235
Camrg <70598949+Camrg@users.noreply.github.com>
2020-12-11 14:16:27 -0500

    about me Assignment 10

--- SemesterProject/app.py
+++ SemesterProject/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 import logging
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     app.logger.warning("dont mess with me")
-    return 'Hello, World!'
+    return render_template('Helloworld.html')
