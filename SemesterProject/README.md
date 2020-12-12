1e51948665377a8c6ad1751b8425d2f84af583b4
Camrg <70598949+Camrg@users.noreply.github.com>
2020-12-11 19:27:55 -0500

    Semester Project Complete

---
+++ SemesterProject/README.md
@@ -0,0 +1,3 @@
+This webpage is a basic site with 4 pages featured. Within, each can navigate back to the home page, as well as on to various other pages. There are pages for home, about, contact, and hobbies. When a page is seached that does not exist, the user will be sent to a 404 page that contains a link to return to the home page. All code should be neatly organized and simply laid out between the templates and a single .py file. Additionally there is logging that produces a message server side to give notice of what pages are being accessed. I hope you enjoy! Thanks for a great semester and all your help and patience!
+
+-Cam
--- SemesterProject/app.py
+++ SemesterProject/app.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import logging
 app = Flask(__name__)
 
+#This is the home page, from here you can access all other pages
 @app.route('/')
-def hello_world():
-    app.logger.warning("don't mess with me")
+@app.route("/home")
+def home():
+    app.logger.warning("Welcome to the home page")
     return render_template('Helloworld.html')
+
+@app.route('/aboutme')
+def aboutme():
+    app.logger.warning("user naviagted to the about me page")
+    return render_template('aboutme.html')
+
+@app.route('/contact')
+def contact():
+    app.logger.warning("user navigated to the contact page")
+    return render_template('contact.html')
+
+@app.route('/myhobbies')
+def myhobbies():
+    app.logger.warning("user navigated to the hobbies page")
+    return render_template('myhobbies.html')
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
